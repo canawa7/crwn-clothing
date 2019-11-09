eae7a5eca5dc44bb3b7a7a594165e798f2de86f2
Calvin Wahyudi <cnwcareer@gmail.com>
2019-11-08 18:10:46 -0800

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -11,6 +11,8 @@ import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.action';
+
+
 
 // const HatsPage = () => (
 //   <div>
@@ -95,18 +97,22 @@ class App extends React.Component{
         <Switch>
           <Route exact={true} path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
         </Switch>
       </div>
     );  
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 
 //exact is a true or false property. This path must be exactly '/' in order for the component to render.
@@ -125,3 +131,5 @@ export default connect(null, mapDispatchToProps)(App);
 
 
 // <Header currentUser={this.state.currentUser}/>
+
+// <Route exact path='/signin' component={SignInAndSignUpPage}/>
